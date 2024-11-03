e54490260be8c7346f55ccdb126c9299f7111e95
vikas kurmi <154617102+vikaskurmi17560@users.noreply.github.com>
2024-11-03 14:57:56 +0000

    ....

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/vikaskurmi17560/'
+  base:'/TechGadgets2.0/'
 })
